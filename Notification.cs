8b63e47afa3475e9472ffd77f2ee8a4046982d1f
Robby Bourne <robbybourne1@gmail.com>
2017-08-16 14:49:44 -0400

    Notification Setup

--- Notification.cs
+++ Notification.cs
@@ -4,6 +4,20 @@ namespace inheritanceDaily_dotnet
 {
     public abstract class Notification
     {
-        
+        public DateTime CreatedAt { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+
+        public Notification(string subject, string body)
+        {
+            Subject = subject;
+            Body = body;
+            CreatedAt = DateTime.Now;
+        }
+
+        public void Transport();
+        {
+            throws(NoTransportException);
+        }
     }
 }
